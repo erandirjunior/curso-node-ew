d4a230ba27ea1c2496336db403ee94b088ef3207
Erandir <antonio.soares@multilaser.com.br>
2022-10-06 21:27:44 -0300

    Mod 10: Adicionando host para funcionar no docker

--- mod-10/index.js
+++ mod-10/index.js
@@ -3,4 +3,4 @@ const http = require('http');
 http.createServer((reqquest, response) => {
 	response.end('Hello Node!');
 })
-.listen(8080, 'localhost', () => console.log('Server up!'));
+.listen(8000, '0.0.0.0', () => console.log('Server up!'));
