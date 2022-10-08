3d0533cfb6eb2b6c617d15be7502f5a7c00102ee
Erandir <antonio.soares@multilaser.com.br>
2022-10-07 21:08:51 -0300

    Mod 10: Listando dados via requisicao http

--- mod-10/src/routes/hero-routes.js
+++ mod-10/src/routes/hero-routes.js
@@ -11,7 +11,23 @@ module.exports = class HeroRoutes extends BaseRoute {
 			path: '/heroes',
 			method: 'GET',
 			handler: (request, headers) => {
-				return this.context.find();
+				try {
+					const {skip, limit, name} = request.query;
+					const query = name ? {name} : {};
+
+					if (skip && !/^\d+$/.test(skip)) {
+						throw Error('Skip type is not accepted');
+					}
+
+					if (limit && !/^\d+$/.test(limit)) {
+						throw Error('Limit type is not accepted');
+					}
+
+					return this.context.find(query, parseInt(skip), parseInt(limit));
+				} catch (e) {
+					console.log(e);
+					return headers.response('Internal Error').code(500);
+				}
 			}
 		}
 	}
--- mod-10/src/tests/api-heroes.js
+++ mod-10/src/tests/api-heroes.js
@@ -22,4 +22,47 @@ describe.only('Api Routes', function() {
 		assert.deepEqual(statusCode, 200);
 		assert.ok(Array.isArray(data));
 	});
+
+	it('List /heroes -> must return 3 registers', async () => {
+		const limit = 3;
+		const result = await app.inject({
+			method: 'GET',
+			url: `/heroes?skip=0&limit=${limit}`
+		});
+
+		// O retorno vem como string
+		// Foi necessário converter para json
+		const data = JSON.parse(result.payload);
+
+		const statusCode = result.statusCode;
+		
+		assert.deepEqual(statusCode, 200);
+		assert.ok(data.length === limit);
+	});
+
+	it('List /heroes -> must return error', async () => {
+		const limit = 3
+		const result = await app.inject({
+			method: 'GET',
+			url: `/heroes?skip=0a&limit=${limit}`
+		});
+		const statusCode = result.statusCode;
+		assert.deepEqual(statusCode, 500);
+	});
+
+	it('List /heroes -> must return only data with specific name', async () => {
+		const name = 'Spider-Man'
+		const result = await app.inject({
+			method: 'GET',
+			url: `/heroes?name=${name}`
+		});
+
+
+		const data = JSON.parse(result.payload);
+		const statusCode = result.statusCode;
+
+		assert.deepEqual(statusCode, 200);
+
+		assert.ok(data.length === 2);
+	});
 });
